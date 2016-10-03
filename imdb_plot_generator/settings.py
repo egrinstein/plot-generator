f231ddb33d286c95de7e67d801104166df0fbd6e
Eric Grinstein <ericgrinstein@gmail.com>
2016-10-04 01:04:42 +0200

    fixes more encoding in settings

--- imdb_plot_generator/settings.py
+++ imdb_plot_generator/settings.py
@@ -128,5 +128,5 @@ STATICFILES_DIRS = (
 
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
-SECURE_PROXY_SSL_HEADER = (‘HTTP_X_FORWARDED_PROTO’, ‘https’)
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO','https')
 
