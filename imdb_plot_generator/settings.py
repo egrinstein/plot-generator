98625799545a1ee8492499a8129f6302779f1899
Eric Grinstein <ericgrinstein@gmail.com>
2016-10-04 01:09:14 +0200

    heroku is annoying

--- imdb_plot_generator/settings.py
+++ imdb_plot_generator/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'imdb_plot_generator.apps.markov',
+    'markov',
     ]
 
 MIDDLEWARE = [
