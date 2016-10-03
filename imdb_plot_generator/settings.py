664797618ccb6c237e7e374e4fac7f62305da20a
Eric Grinstein <ericgrinstein@gmail.com>
2016-10-04 00:56:05 +0200

    Static path corrected

--- imdb_plot_generator/settings.py
+++ imdb_plot_generator/settings.py
@@ -118,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = (
+            os.path.join(BASE_DIR, 'static'),
+            )
 
 # Heroku Postgres 
 
